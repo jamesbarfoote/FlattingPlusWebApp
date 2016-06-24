263ec2c81f643371facac91137b6e384bfab4157
James Barfoote <james.barfoote@gmail.com>
2016-06-24 21:16:38 +1200

    uncommented

--- flattingplusweb/app.js
+++ flattingplusweb/app.js
@@ -38,7 +38,7 @@ app.use("/", express.static(__dirname + '/public'));//serve up the website
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
