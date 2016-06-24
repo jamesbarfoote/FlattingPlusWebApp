88e18b096808a8613754051e63f615a3c74a16a2
James Barfoote <james.barfoote@gmail.com>
2016-06-24 21:47:28 +1200

    fixed up the package.json

--- flattingplusweb/server.js
+++ flattingplusweb/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -38,7 +38,7 @@ app.use("/", express.static(__dirname + '/public'));//serve up the website
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
